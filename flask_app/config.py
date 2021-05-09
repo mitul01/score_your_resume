608b57eb60673d539af98bda54c608eeffc42b40
Mitul <mitultandon56@gmail.com>
2021-05-10 01:26:24 +0530

    config url

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,3 +1,4 @@
+from os import environ
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')  or 'sqlite:///resume.db'
