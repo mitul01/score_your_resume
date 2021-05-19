e4c8412ae6d981ec586b2c231810a71fb9ebf0af
Mitul <mitultandon56@gmail.com>
2021-05-20 00:11:06 +0530

    fix sql path

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,4 +1,4 @@
 from os import environ
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')  or 'sqlite:///resume.db'
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL_SQL')  or 'sqlite:///resume.db'
