9b0ffaa63f3c7a9fb923cf21f7750533c4003987
Mitul <mitultandon56@gmail.com>
2021-05-19 22:23:00 +0530

    fix zero error

--- flask_app/score.py
+++ flask_app/score.py
@@ -120,7 +120,7 @@ class ScoreResume:
         nlp = spacy.load("en_core_web_sm")
         doc = nlp(text)
         sents = list(doc.sents)
-        count_passive=0
+        count_passive=1
         for s in sents:
             passive=av.is_passive(str(s))
             if passive:
@@ -152,10 +152,13 @@ class ScoreResume:
     def quantifier_score(self):
         text=self.get_file_text()
         text=self.clean_text(text)
+        URL_REGEX =
+        r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+        links-re.findall(URL_REGEX,text)
         numbers = re.findall('[0-9]+', text)
         signs = re.findall(r'%', text)
         scaled_quant_score=self.range_score(output_start=0,output_end=100,
-                                input_start=0,input_end=len(text.split(' '))/6,input=len(numbers)+len(signs))
+                                input_start=0,input_end=len(text.split(' '))/6,input=len(numbers)+len(signs)+len(links))
         return round(scaled_quant_score)
 
 
@@ -163,8 +166,8 @@ class ScoreResume:
         text=self.get_file_text()
         text=self.clean_text(text)
         # Calculating General Score
-        gen_points_scaled=0
-        gen_points=0
+        gen_points_scaled=1
+        gen_points=1
         keyword_match=[]
         general_keyword={'certifications':1,'experience':1,'skills':1,
                         'voluntary':2,'specialist':1,'knowledge':1,'exceptional':1,
@@ -205,8 +208,8 @@ class ScoreResume:
             points_dict={''}
 
         match_career_keywords=[]
-        special_points=0
-        scaled_special_points=0
+        special_points=1
+        scaled_special_points=1
         for t in text.split(" "):
             match=self.closeMatches(list(points_dict.keys()),t)
             if match!=[]:
